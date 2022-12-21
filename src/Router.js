5462b71116da9e9642bf586fce3ad433d7914dc8
Mia <tjwlfif@gmail.com>
2022-12-21 12:48:35 +0900

    directory 정리

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path="" element={} /> */}</Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
