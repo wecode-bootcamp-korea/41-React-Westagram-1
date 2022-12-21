94e73123616a0b07ca002cda8c2fd027b0f51558
phs7973 <phs7973@naver.com>
2022-12-21 16:46:44 +0900

    41_hwanseong:폴더 및 파일 추가

---
+++ src/pages/41_hwanseong/Login/Login.js
@@ -0,0 +1,7 @@
+import React from 'React'
+import './Login.scss';
+function Login() {
+  return <h2>Login</h2>
+  
+}
+export default 'Login'
---
+++ src/pages/41_hwanseong/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'React'
+import './Main.scss';
+function Main() {
+  return <h2>Main</h2>
+}
+
+export default 'main'
