bfcf4ca23c17bcb83d790a3cc0b785d7c335502c
sally-dot <doooit135@gmail.com>
2022-12-21 16:41:50 +0900

    41_dahye 수정 push

---
+++ src/pages/41_dahye/Login/Login.js
@@ -0,0 +1,11 @@
+import "./Login.scss";
+
+const Login () => {
+  return (
+    <>
+      <h1>Lee Dahye Login-Page</h1>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/pages/41_dahye/Main/Main.js
@@ -0,0 +1,11 @@
+import "./Main.scss";
+
+const Main () => {
+  return (
+    <>
+      <h1>Lee Dahye Maing-Page</h1>
+    </>
+  );
+}
+
+export default Main;
