ca061e4c57ef5382e639af62bfe715fa32cb42bd
dagyeong Jeong <jdk09140@gmail.com>
2022-12-21 16:41:35 +0900

    ADD: Main, Login js,cscc 파일 생성

---
+++ src/pages/41_dagyeong/Login/Login.js
@@ -0,0 +1,7 @@
+import './Login.scss';
+
+function Login() {
+  return <h1>안냐세여 로그인페이지 입니뎅~</h1>;
+}
+
+export default Login;
---
+++ src/pages/41_dagyeong/Main/Main.js
@@ -0,0 +1,7 @@
+import './Main.scss';
+
+function Main() {
+  return <h1>안냐세여 메인페이지 입니뎅</h1>;
+}
+
+export default Main;
