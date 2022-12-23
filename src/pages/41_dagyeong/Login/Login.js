e292a04cbc4b28f23ea4fb4f5bab471e2fd1b0cb
dagyeong Jeong <jdk09140@gmail.com>
2022-12-23 15:59:43 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/41_dagyeong/Login/Login.js
+++ src/pages/41_dagyeong/Login/Login.js
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function LoginDagyeong() {
-  return <h1>안냐세여 로그인페이지 입니뎅~</h1>;
+  const navigate = useNavigate();
+
+  const [idValue, setId] = useState('');
+  const [pwValue, setPw] = useState('');
+  // const [disabled, setDisabled] = useState(false);
+  // const [color, setColor] = useState("#596d99");
+
+  const saveUserId = event => {
+    setId(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  const saveUserPw = event => {
+    setPw(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  // const isDisabled = (idValue) => {
+  //   idValue.includes("@") && pwValue.length >= 5
+  //     ? setDisabled(false)
+  //     : setDisabled(true);
+  // };
+
+  return (
+    <div className="login_wraaper">
+      <div className="login">
+        <h2>Westagram</h2>
+        <div className="login_box">
+          <input
+            className="login_id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            value={idValue}
+            onChange={saveUserId}
+          />
+          <input
+            className="login_pw"
+            type="password"
+            placeholder="비밀번호"
+            value={pwValue}
+            onChange={saveUserPw}
+          />
+          <div>
+            <button
+              className="login_btn"
+              onClick={() => {
+                navigate('/main');
+              }}
+            >
+              로그인
+            </button>
+          </div>
+        </div>
+        <div className="forget_pw">
+          <p>비밀번호를 잊으셨나요?</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginDagyeong;
--- src/pages/41_dagyeong/Main/Main.js
+++ src/pages/41_dagyeong/Main/Main.js
@@ -2,7 +2,203 @@ import React from 'react';
 import './Main.scss';
 
 function MainDagyeong() {
-  return <h1>안냐세여 메인페이지 입니뎅</h1>;
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar_left">
+          <img
+            className="icon"
+            src="./images/41_dagyeong/instagram.png"
+            alt="instagram logo"
+          />
+          <span>Westagram</span>
+        </div>
+        <input type="text" placeholder="검색" className="navbar_search" />
+        <div className="navbar_icons">
+          <img
+            className="icon"
+            src="./images/41_dagyeong/explore.png"
+            alt="explore"
+          />
+          <img
+            className="icon"
+            src="./images/41_dagyeong/heart.png"
+            alt="heart"
+          />
+          <img
+            className="icon"
+            src="./images/41_dagyeong/profile.png"
+            alt="profile"
+          />
+        </div>
+      </nav>
+      <main>
+        <div className="feeds">
+          <article>
+            <div className="feedTop">
+              <img
+                src="./images/41_dagyeong/myProfile.jpeg"
+                className="feedprofile_img"
+                alt="my profile"
+              />
+              <span className="feeds_nickName">hihello</span>
+              <img
+                src="./images/41_dagyeong/option.png"
+                className="feedTop_option"
+                alt="menu"
+              />
+            </div>
+            <img
+              src="./images/41_dagyeong/pic2.jpeg"
+              className="feeds_img"
+              alt="picture1"
+            />
+            <div className="article_icons">
+              <img src="./images/41_dagyeong/heart.png" alt="heart" />
+              <img src="./images/41_dagyeong/chat.png" alt="chat" />
+              <img src="./images/41_dagyeong/send.png" alt="send" />
+              <img src="./images/41_dagyeong/bookmark.png" alt="bookmark" />
+            </div>
+            <p className="like">좋아요 3270개</p>
+            <ul id="commentLists">
+              <li className="comment">
+                <span>nana123</span>
+                <span>위워크에서 진행한 필름카메라 클래스...</span>
+                {/* <!-- <img src="./images/heart.png" class="comment_icon" /> --> */}
+                <span className="delete">x</span>
+              </li>
+              <li className="comment">
+                <span>spam789</span>
+                <span>나도 필름카메라 가지고 싶다!!</span>
+                <span className="delete">x</span>
+                {/* <!-- <img src="./images/heart.png" class="comment_icon" /> --> */}
+              </li>
+            </ul>
+            <p className="like">35분전</p>
+            <hr />
+            {/* <!-- 댓글 --> */}
+            <div className="do_comment">
+              <input
+                type="text"
+                placeholder="댓글달기..."
+                className="comment_input"
+              />
+              <button className="comment_btn" id="post">
+                게시
+              </button>
+            </div>
+          </article>
+          {/* <!-- 메인 오른쪽 페이지 --> */}
+          <div className="main_right">
+            <div className="myprofile">
+              <img
+                src="./images/41_dagyeong/myProfile.jpeg"
+                className="myprofile_img"
+                alt="my profile"
+              />
+              <div className="my_profile">
+                <span>nana123</span>
+                <span>na | 인스타그램</span>
+              </div>
+            </div>
+            {/* <!-- 스토리 --> */}
+            <div className="storybox">
+              <div className="storybox_top">
+                <span>스토리</span>
+                <span className="story_all_view">모두 보기</span>
+              </div>
+              <div className="westagram_story">
+                <div className="story_nickname">
+                  <img
+                    src="./images/41_dagyeong/friend1.jpeg"
+                    className="feedprofile_img"
+                    alt="feed profile1"
+                  />
+                  <div className="story_state">
+                    <p>my_me_mine</p>
+                    <p className="story_time">2분전</p>
+                  </div>
+                </div>
+                <div className="story_nickname">
+                  <img
+                    src="./images/41_dagyeong/friend2.jpeg"
+                    className="feedprofile_img"
+                    alt="feed profile2"
+                  />
+                  <div className="story_state">
+                    <p>ひらがな</p>
+                    <p className="story_time">12분전</p>
+                  </div>
+                </div>
+                <div className="story_nickname">
+                  <img
+                    src="./images/41_dagyeong/friend1.jpeg"
+                    className="feedprofile_img"
+                    alt="feed profile3"
+                  />
+                  <div className="story_state">
+                    <p>Zhōngguó rén</p>
+                    <p className="story_time">12분전</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <!-- 회원 추천 --> */}
+            <div className="recommend">
+              <div className="recommend_top">
+                <span>회원님을 위한 추천</span>
+                <span className="recommend_all_view">모두 보기</span>
+              </div>
+              <div className="story_nickname">
+                <img
+                  src="./images/41_dagyeong/friend1.jpeg"
+                  className="feedprofile_img"
+                  alt="recommend friend"
+                />
+                <div className="story_state">
+                  <p>happy</p>
+                  <p className="story_anotherpeople">_dfasf 님 외 2명이...</p>
+                </div>
+                <div className="recommend_follow">
+                  <button>팔로우</button>
+                </div>
+              </div>
+              <div className="story_nickname">
+                <img
+                  src="./images/41_dagyeong/friend1.jpeg"
+                  className="feedprofile_img"
+                  alt="feed profile"
+                />
+                <div className="story_state">
+                  <p>looks_good</p>
+                  <p className="story_anotherpeople">o_o 님 외 6명이...</p>
+                </div>
+                <div className="recommend_follow">
+                  <button>팔로우</button>
+                </div>
+              </div>
+              <div className="story_nickname">
+                <img
+                  src="./images/41_dagyeong/friend1.jpeg"
+                  className="feedprofile_img"
+                  alt="feed profile"
+                />
+                <div className="story_state">
+                  <p>joy</p>
+                  <p className="story_anotherpeople">
+                    Zhōngguó rén 님 외 2명이...
+                  </p>
+                </div>
+                <div className="recommend_follow">
+                  <button>팔로우</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default MainDagyeong;
