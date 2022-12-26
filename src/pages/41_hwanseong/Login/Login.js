bb251fc111bdf9253236f54c144645f8955e94da
phs7973 <phs7973@naver.com>
2022-12-26 16:55:59 +0900

    로그인 메인 페이지 옮김

--- src/pages/41_hwanseong/Login/Login.js
+++ src/pages/41_hwanseong/Login/Login.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './Login.scss';
 
 function LoginHwanseong() {
-  return <h2>Login</h2>;
+  return (
+    <div className="body">
+      <header>Westagram</header>
+      <input
+        className="inputId"
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+      />
+      <input className="inputPassword" type="password" placeholder="비밀번호" />
+      <button className="inputButton" type="button" disabled>
+        로그인
+      </button>
+      <footer>비밀번호를 잊으셨나요?</footer>
+      <script type="text/javascript" src="login.js" />
+    </div>
+  );
 }
 export default LoginHwanseong;
--- src/pages/41_hwanseong/Main/Main.js
+++ src/pages/41_hwanseong/Main/Main.js
@@ -1,8 +1,142 @@
 import React from 'react';
 import './Main.scss';
 
-function MainHwanseong() {
-  return <h2>Main</h2>;
-}
+const Main = () => {
+  return (
+    <section className="main">
+      <div className="nav">
+        <div className="navLeft">
+          <img src="images/41_hwanseong/mainpagelogo.png" alt="로고" />
+          <span>westagram</span>
+        </div>
+        <div className="navCenter">
+          <input placeholder="검색" />
+        </div>
+        <div className="navRight">
+          <img src="images/41_hwanseong/compass.png" alt="컴퍼스" />
+          <img src="images/41_hwanseong/heart.png" alt="하트" />
+          <img src="images/41_hwanseong/user.png" alt="사람" />
+        </div>
+      </div>
+      <main className="mainContainer">
+        <div className="feeds">
+          <div className="article">
+            <div className="articleTop">
+              <img
+                className="userPp"
+                src="images/41_hwanseong/userImage.jpg"
+                alt="유저프사"
+              />
+              <div className="userId">
+                <div>Wecode_bootcamp</div>
+                <div>wecode</div>
+              </div>
+              <img
+                className="jeomsegae"
+                src="images/41_hwanseong/list.png"
+                alt="점세개"
+              />
+            </div>
+            <div className="articleMid">
+              <img
+                className="articleImage"
+                src="images/41_hwanseong/articleImage.jpg"
+                alt="아티클이미지"
+              />
+            </div>
+            <div className="articleBot">
+              <div className="imagesegae">
+                <img src="images/41_hwanseong/heart.png" alt="좋아요" />
+                <img src="images/41_hwanseong/list.png" alt="멘션" />
+                <img src="images/41_hwanseong/compass.png" alt="다운로드" />
+                <div className="flag">
+                  <img src=".images/sad.png" alt="!" />
+                </div>
+              </div>
+              <div>블라블라블라블라</div>
+              <input placeholder="댓글입력" />
+            </div>
+          </div>
+        </div>
+        <div className="mainRight">
+          <div className="mainRighttop">
+            <img
+              className="userPp2"
+              src="./images/userImage.jpg"
+              alt="또프사"
+            />
+            <div className="userId2">
+              <div>Wecode_bootcamp</div>
+              <div>wecode</div>
+            </div>
+          </div>
+          <div className="mainRightmid">
+            <div>
+              <div className="userId3">
+                <img
+                  className="userPp3"
+                  src="./images/userImage.jpg"
+                  alt="중복프사"
+                />
+                <div>
+                  <div>Wecode_bootcamp</div>
+                  <div>wecode</div>
+                </div>
+              </div>
+              <div className="userId3">
+                <img
+                  className="userPp3"
+                  src="./images/userImage.jpg"
+                  alt="중복프사"
+                />
+                <div>
+                  <div>Wecode_bootcamp</div>
+                  <div>wecode</div>
+                </div>
+              </div>
+              <div className="userId3">
+                <img
+                  className="userPp3"
+                  src="./images/userImage.jpg"
+                  alt="중복프사"
+                />
+                <div>
+                  <div>Wecode_bootcamp</div>
+                  <div>wecode</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mainRightbot">
+            <div className="b">
+              <div className="userId4">
+                <img
+                  className="userPp4"
+                  src="./images/userImage.jpg"
+                  alt="또중복프사"
+                />
+                <div>
+                  <div>Wecode_bootcamp</div>
+                  <div>wecode</div>
+                </div>
+              </div>
+              <div className="userId4">
+                <img
+                  className="userPp4"
+                  src="./images/userImage.jpg"
+                  alt="또중복프사"
+                />
+                <div>
+                  <div>Wecode_bootcamp</div>
+                  <div>wecode</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
 
-export default MainHwanseong;
+export default Main;
