c7963f8c0ddfd6b39e3f959c6682722e6995d4a7
Mia <tjwlfif@gmail.com>
2022-12-26 15:15:47 +0900

    Refactor: import 순서 변경

--- src/pages/41_jiyeonSeo/Main/Main_re.js
+++ src/pages/41_jiyeonSeo/Main/Main_re.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Main_re.scss';
-import '../styles/common.scss';
-import '../styles/variables.scss';
 import Nav from './MainComponents/Nav';
 import Content from './MainComponents/Content';
 import MainRight from './MainComponents/MainRight';
+import './Main_re.scss';
+import '../styles/common.scss';
+import '../styles/variables.scss';
 
 function Main() {
   return (
