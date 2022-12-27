57be09b48f4f648e6ed9213db1d9cefbd9277564
Mia <tjwlfif@gmail.com>
2022-12-27 20:50:35 +0900

    Modify: 메인 피드 부분 컴포넌트화 완료 및 useEffect 수정

--- src/pages/41_jiyeonSeo/Main/MainComponents/Content.js
+++ src/pages/41_jiyeonSeo/Main/MainComponents/Content.js
@@ -35,6 +35,13 @@ function Comment({ name, text, liked }) {
 }
 
 function Feed({ name, profileSrc, src, text, liked }) {
+  const [commentLists, setCommentLists] = useState([]);
+  useEffect(() => {
+    fetch('data/41_jiyeon/commentData.json')
+      .then(Response => Response.json())
+      .then(result => setCommentLists(result));
+  }, []);
+
   const [input, setInput] = useState('');
   const [comments, setComments] = useState([]);
   const [like, setLike] = useState(liked);
@@ -106,8 +113,26 @@ function Feed({ name, profileSrc, src, text, liked }) {
         </p>
         <div>
           <ul className="comment-lists">
+            <Comment
+              name="wecode_bootcamp"
+              text="여기서 이러시면 안됩니다!!"
+              liked={true}
+            />
+            {commentLists.map(comment => (
+              <Comment
+                key={comment.id}
+                name={comment.name}
+                text={comment.text}
+                liked={comment.liked}
+              />
+            ))}
             {comments.map((comment, index) => (
-              <Comment key={index} name={name} text={comment} liked={false} />
+              <Comment
+                key={index}
+                name="miya___ho"
+                text={comment}
+                liked={false}
+              />
             ))}
           </ul>
         </div>
@@ -132,147 +157,22 @@ function Feed({ name, profileSrc, src, text, liked }) {
 }
 
 function Content() {
-  /*mock data로 들어온 commnetList*/
-  const [commentLists, setCommentLists] = useState([]);
-  /*mock data로 들어온 feedList*/
   const [feedLists, setFeedLists] = useState([]);
-
   useEffect(() => {
-    fetch('./data/41_jiyeon/commentData.json', {
-      method: 'GET',
-    })
-      .then(Response => Response.json())
-      .then(result => setCommentLists(result));
-  });
-
-  useEffect(() => {
-    fetch('./data/41_jiyeon/feedData.json', {
-      method: 'GET',
-    })
+    fetch('data/41_jiyeon/feedData.json')
       .then(Response => Response.json())
       .then(result => setFeedLists(result));
-  });
-
-  const [input, setInput] = useState('');
-  const [comments, setComments] = useState([]);
-  const [like, setLike] = useState(true);
-  let readyToClick = input !== '' ? true : false;
-
-  const onChange = event => {
-    setInput(event.target.value);
-  };
-
-  const onSubmit = event => {
-    event.preventDefault();
-    if (input === '') {
-      return;
-    }
-    setComments(prev => [...prev, input]);
-    setInput('');
-  };
-
-  const handleHeartClick = event => {
-    setLike(prev => !prev);
-  };
+  }, []);
 
   return (
     <div className="content">
-      <div className="feed">
-        <div className="feed-top">
-          <img
-            className="profile-photo"
-            src="./images/41_jiyeon/mia.png"
-            alt="프로필 이미지"
-          />
-          <p>mia_seo</p>
-          <img src="./images/41_jiyeon/dot.png" alt="dot" />
-        </div>
-        <img
-          className="feed-img"
-          src="./images/41_jiyeon/붕어빵루피.jpeg"
-          alt="붕어빵루피"
-        />
-        <div className="like-emojis">
-          <img
-            onClick={handleHeartClick}
-            src={
-              like
-                ? './images/41_jiyeon/heart-fill.png'
-                : './images/41_jiyeon/heart.png'
-            }
-            alt="하트"
-          />
-          <img
-            className="small-emojis"
-            src="./images/41_jiyeon/bubble-empty.png"
-            alt="댓글"
-          />
-          <img
-            className="small-emojis"
-            src="./images/41_jiyeon/upload (1).png"
-            alt="업로드"
-          />
-          <img
-            className="small-emojis"
-            src="./images/41_jiyeon/bookmark.png"
-            alt="북마크"
-          />
-        </div>
-        <p className="txt-likes">
-          <img
-            className="small-image"
-            src="./images/41_jiyeon/mia.png"
-            alt="프로필 이미지"
-          />
-          <span className="bold">mia_seo</span>님
-          <span className="bold">외 100명</span>이 좋아합니다
-        </p>
-        <div className="comments">
-          <p>
-            <span className="bold">mia_seo</span> 인생... 힘들다....💧
-          </p>
-          <div>
-            <ul className="comment-lists">
-              <Comment
-                name="wecode_bootcamp"
-                text="여기서 이러시면 안됩니다!!"
-                liked={true}
-              />
-              {commentLists.map(comment => (
-                <Comment
-                  key={comment.id}
-                  name={comment.name}
-                  text={comment.text}
-                  liked={comment.liked}
-                />
-              ))}
-              {comments.map((comment, index) => (
-                <Comment
-                  key={index}
-                  name="miya___ho"
-                  text={comment}
-                  liked={false}
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
-        <form id="comment" onSubmit={onSubmit}>
-          <input
-            onChange={onChange}
-            id="commentInput"
-            type="text"
-            placeholder="댓글달기"
-            required
-            value={input}
-          />
-          <button
-            style={readyToClick ? { color: '#2099f1' } : { color: '#c5e1fb' }}
-          >
-            게시
-          </button>
-        </form>
-      </div>
+      <Feed
+        name="mia_seo"
+        profileSrc="./images/41_jiyeon/mia.png"
+        src="./images/41_jiyeon/붕어빵루피.jpeg"
+        text="인생... 힘들다....💧"
+        liked={true}
+      />
       {feedLists.map(feed => (
         <Feed
           key={feed.id}
